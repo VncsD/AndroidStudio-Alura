f3338f2add37dab8da46bcf1ccab99d748b01f8a
ViniciusD <vncs.du@gmail.com>
2019-10-08 14:20:43 -0300

    Implementando um ListView e AdapterArray

--- app/src/main/java/br/com/alura/agenda/MainActivity.java
+++ app/src/main/java/br/com/alura/agenda/MainActivity.java
@@ -2,9 +2,16 @@ package br.com.alura.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -12,5 +19,10 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> alunos = new ArrayList(Arrays.asList("Vini","Re","Ti","La","Pa","RaRa"));
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this,android.R.layout.simple_expandable_list_item_1, alunos));
+
     }
 }
